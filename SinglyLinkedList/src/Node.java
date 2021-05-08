aa8b262134e834f8741d1dc28c5c102e82dbb72d
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-08 15:27:13 +0530

    Initialize git repo

---
+++ SinglyLinkedList/src/Node.java
@@ -0,0 +1,26 @@
+public class Node {
+    private int data;
+    private Node nextNode;
+
+    public Node(int data, Node nextNode) {
+        this.setData(data);
+        this.setNextNode(nextNode);
+    }
+
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+}
