1a76e5839336c73a2fe40e7776ca19fc8095cb10
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-08 16:56:06 +0530

    Create SinglyLinkedListTest class

---
+++ SinglyLinkedList/src/SinglyLinkedListTest.java
@@ -0,0 +1,12 @@
+import ds.SinglyLinkedList;
+
+public class SinglyLinkedListTest {
+
+    public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        System.out.println(singlyLinkedList.empty());;
+        singlyLinkedList.print();
+        System.out.println(singlyLinkedList.size());
+        singlyLinkedList.clear();
+    }
+}
